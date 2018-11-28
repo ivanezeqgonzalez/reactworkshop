fe38553d8bb05a46344ee7c22f16e442117de16e
Ivan Gonzalez <ivanezeqgonzalez@github.com>
2018-11-28 17:47:50 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  // Agregamos el constructor y definimos el estado
+  constructor(props){
+    super(props);
+    this.state = {
+      clima: {}
+    };
+  }
+  // Definimos el método componentDidMount con el fetch
+  componentDidMount() {
+    fetch(
+      `https://api.apixu.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=Buenos+Aires&days=6`
+    )
+      .then(response => response.json())
+      .then(jsonData => {
+        this.setState({ clima: jsonData });
+      });
+  }
+  
   render() {
+    const { location, current, forecast } = this.state.clima;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <div className="top">
+            <img src={logo} alt="Clima principal" className="image" />
+            <p className="temp">25 °C</p>
+            <h2 className="city">Buenos Aires, CABA, Argentina</h2>
+            <div className="feelslike">ST: 25.4 °C, H: 81 %</div>
+          </div>
+          <div className="bottom">
+            <div className="card">
+              <h5>Lunes</h5>
+              <h6>2018-09-26</h6>
+              <img src={logo} alt="Clima diario" className="imagebottom" />
+              <div className="minmax">
+                <p>
+                  <span className="tempmax" />
+                  26
+                </p>
+                <p>
+                  <span className="tempmin" />
+                  15
+                </p>
+              </div>
+            </div>
+            <div className="card">
+              <h5>Martes</h5>
+              <h6>2018-09-27</h6>
+              <img src={logo} alt="Clima diario" className="imagebottom" />
+              <div className="minmax">
+                <p>
+                  <span className="tempmax" />
+                  26
+                </p>
+                <p>
+                  <span className="tempmin" />
+                  15
+                </p>
+              </div>
+            </div>
+            <div className="card">
+              <h5>Miércoles</h5>
+              <h6>2018-09-28</h6>
+              <img src={logo} alt="Clima diario" className="imagebottom" />
+              <div className="minmax">
+                <p>
+                  <span className="tempmax" />
+                  26
+                </p>
+                <p>
+                  <span className="tempmin" />
+                  15
+                </p>
+              </div>
+            </div>
+            <div className="card">
+              <h5>Jueves</h5>
+              <h6>2018-09-29</h6>
+              <img src={logo} alt="Clima diario" className="imagebottom" />
+              <div className="minmax">
+                <p>
+                  <span className="tempmax" />
+                  26
+                </p>
+                <p>
+                  <span className="tempmin" />
+                  15
+                </p>
+              </div>
+            </div>
+            <div className="card">
+              <h5>Viernes</h5>
+              <h6>2018-09-30</h6>
+              <img src={logo} alt="Clima diario" className="imagebottom" />
+              <div className="minmax">
+                <p>
+                  <span className="tempmax" />
+                  26
+                </p>
+                <p>
+                  <span className="tempmin" />
+                  15
+                </p>
+              </div>
+            </div>
+            <div className="card">
+              <h5>Sábado</h5>
+              <h6>2018-10-01</h6>
+              <img src={logo} alt="Clima diario" className="imagebottom" />
+              <div className="minmax">
+                <p>
+                  <span className="tempmax" />
+                  26
+                </p>
+                <p>
+                  <span className="tempmin" />
+                  15
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Saludo.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Saludo = (props) => <p>Hola {props.nombre}</p>;
+
+export default Saludo;
