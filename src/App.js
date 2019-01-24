6169342ee0061489bb397ba0bd4bd20a2084e0e0
ivanezeqgonzalez <ivanezeqgonzalez@github.com>
2019-01-24 16:50:32 -0300

    it's works

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      clima: {}
+      clima: {},
+      load: false
     };
   }
   // Definimos el método componentDidMount con el fetch
@@ -15,28 +16,36 @@ class App extends Component {
     fetch(
       `https://api.apixu.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=Buenos+Aires&days=6`
     )
-      .then(response => response.json())
-      .then(jsonData => {
-        this.setState({ clima: jsonData });
-      });
+    .then(response => response.json())
+    .then(jsonData => {
+        //Manipulamos el string con la imagen para que sea de 128x128 y quede mejor
+        jsonData.current.condition.icon = jsonData.current.condition.icon.replace(
+        "64x64",
+        "128x128"
+      );
+      this.setState({
+         clima: jsonData,
+          load: true 
+        })
+    })
   }
   
   render() {
     const { location, current, forecast } = this.state.clima;
-    return (
+    return !this.state.load ? (<div>Wait</div>) :(
       <div className="App">
         <div className="container">
           <div className="top">
-            <img src={logo} alt="Clima principal" className="image" />
+            <img src={current.condition.icon} alt="Clima principal" className="image" />
             <p className="temp">25 °C</p>
-            <h2 className="city">Buenos Aires, CABA, Argentina</h2>
+            <h2 className="city">{location.name}</h2>
             <div className="feelslike">ST: 25.4 °C, H: 81 %</div>
           </div>
           <div className="bottom">
             <div className="card">
               <h5>Lunes</h5>
               <h6>2018-09-26</h6>
-              <img src={logo} alt="Clima diario" className="imagebottom" />
+              <img src={current.condition.icon} alt="Clima diario" className="imagebottom" />
               <div className="minmax">
                 <p>
                   <span className="tempmax" />
@@ -51,7 +60,7 @@ class App extends Component {
             <div className="card">
               <h5>Martes</h5>
               <h6>2018-09-27</h6>
-              <img src={logo} alt="Clima diario" className="imagebottom" />
+              <img src={current.condition.icon} alt="Clima diario" className="imagebottom" />
               <div className="minmax">
                 <p>
                   <span className="tempmax" />
@@ -66,7 +75,7 @@ class App extends Component {
             <div className="card">
               <h5>Miércoles</h5>
               <h6>2018-09-28</h6>
-              <img src={logo} alt="Clima diario" className="imagebottom" />
+              <img src={current.condition.icon} alt="Clima diario" className="imagebottom" />
               <div className="minmax">
                 <p>
                   <span className="tempmax" />
@@ -81,7 +90,7 @@ class App extends Component {
             <div className="card">
               <h5>Jueves</h5>
               <h6>2018-09-29</h6>
-              <img src={logo} alt="Clima diario" className="imagebottom" />
+              <img src={current.condition.icon} alt="Clima diario" className="imagebottom" />
               <div className="minmax">
                 <p>
                   <span className="tempmax" />
@@ -96,7 +105,7 @@ class App extends Component {
             <div className="card">
               <h5>Viernes</h5>
               <h6>2018-09-30</h6>
-              <img src={logo} alt="Clima diario" className="imagebottom" />
+              <img src={current.condition.icon} alt="Clima diario" className="imagebottom" />
               <div className="minmax">
                 <p>
                   <span className="tempmax" />
@@ -111,7 +120,7 @@ class App extends Component {
             <div className="card">
               <h5>Sábado</h5>
               <h6>2018-10-01</h6>
-              <img src={logo} alt="Clima diario" className="imagebottom" />
+              <img src={current.condition.icon} alt="Clima diario" className="imagebottom" />
               <div className="minmax">
                 <p>
                   <span className="tempmax" />
