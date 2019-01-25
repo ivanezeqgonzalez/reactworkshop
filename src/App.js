abae211b29fcc928e4a418cab880656b31368927
Ivan Gonzalez <ivanezeqgonzalez@github.com>
2019-01-25 12:39:43 -0300

    Create Top Component

--- src/App.js
+++ src/App.js
@@ -1,142 +1,82 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Top from './Top'
+
+const semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado",
+"Domingo"]
 
 class App extends Component {
-  // Agregamos el constructor y definimos el estado
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       clima: {},
-      load: false
+      isLoaded: false
     };
+    //No hay que olvidarse del binding
+    this.getWeekDay = this.getWeekDay.bind(this);
   }
-  // Definimos el método componentDidMount con el fetch
+
+  getWeekDay(fecha) {
+    return semana[new Date(fecha * 1000).getDay()];
+  }
+
   componentDidMount() {
     fetch(
-      `https://api.apixu.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=Buenos+Aires&days=6`
+      `https://api.apixu.com/v1/forecast.json?key=${
+        process.env.REACT_APP_API_KEY
+      }&q=Buenos+Aires&days=6`,
     )
-    .then(response => response.json())
-    .then(jsonData => {
-        //Manipulamos el string con la imagen para que sea de 128x128 y quede mejor
+      .then(response => response.json())
+      .then(jsonData => {
         jsonData.current.condition.icon = jsonData.current.condition.icon.replace(
-        "64x64",
-        "128x128"
-      );
-      this.setState({
-         clima: jsonData,
-          load: true 
+          '64x64',
+          '128x128',
+        )
+        this.setState({
+          clima: jsonData,
+          isLoaded: true,
         })
-    })
+      })
   }
-  
+
   render() {
-    const { location, current, forecast } = this.state.clima;
-    return !this.state.load ? (<div>Wait</div>) :(
+    const { location, current, forecast } = this.state.clima
+    return this.state.isLoaded ? (
       <div className="App">
         <div className="container">
-          <div className="top">
-            <img src={current.condition.icon} alt="Clima principal" className="image" />
-            <p className="temp">25 °C</p>
-            <h2 className="city">{location.name}</h2>
-            <div className="feelslike">ST: 25.4 °C, H: 81 %</div>
-          </div>
+          <Top current={current} location={location}/>
           <div className="bottom">
-            <div className="card">
-              <h5>Lunes</h5>
-              <h6>2018-09-26</h6>
-              <img src={current.condition.icon} alt="Clima diario" className="imagebottom" />
-              <div className="minmax">
-                <p>
-                  <span className="tempmax" />
-                  26
-                </p>
-                <p>
-                  <span className="tempmin" />
-                  15
-                </p>
-              </div>
-            </div>
-            <div className="card">
-              <h5>Martes</h5>
-              <h6>2018-09-27</h6>
-              <img src={current.condition.icon} alt="Clima diario" className="imagebottom" />
-              <div className="minmax">
-                <p>
-                  <span className="tempmax" />
-                  26
-                </p>
-                <p>
-                  <span className="tempmin" />
-                  15
-                </p>
-              </div>
-            </div>
-            <div className="card">
-              <h5>Miércoles</h5>
-              <h6>2018-09-28</h6>
-              <img src={current.condition.icon} alt="Clima diario" className="imagebottom" />
-              <div className="minmax">
-                <p>
-                  <span className="tempmax" />
-                  26
-                </p>
-                <p>
-                  <span className="tempmin" />
-                  15
-                </p>
-              </div>
-            </div>
-            <div className="card">
-              <h5>Jueves</h5>
-              <h6>2018-09-29</h6>
-              <img src={current.condition.icon} alt="Clima diario" className="imagebottom" />
-              <div className="minmax">
-                <p>
-                  <span className="tempmax" />
-                  26
-                </p>
-                <p>
-                  <span className="tempmin" />
-                  15
-                </p>
-              </div>
-            </div>
-            <div className="card">
-              <h5>Viernes</h5>
-              <h6>2018-09-30</h6>
-              <img src={current.condition.icon} alt="Clima diario" className="imagebottom" />
-              <div className="minmax">
-                <p>
-                  <span className="tempmax" />
-                  26
-                </p>
-                <p>
-                  <span className="tempmin" />
-                  15
-                </p>
-              </div>
-            </div>
-            <div className="card">
-              <h5>Sábado</h5>
-              <h6>2018-10-01</h6>
-              <img src={current.condition.icon} alt="Clima diario" className="imagebottom" />
-              <div className="minmax">
-                <p>
-                  <span className="tempmax" />
-                  26
-                </p>
-                <p>
-                  <span className="tempmin" />
-                  15
-                </p>
-              </div>
-            </div>
+            {forecast.forecastday.map(dia => {
+              return (
+                <div className="card" key={dia.date}>
+                  <h5>{this.getWeekDay(dia.date_epoch)}</h5>
+                  <h6>{dia.date}</h6>
+                  <img
+                    src={dia.day.condition.icon}
+                    alt="Clima diario"
+                    className="imagebottom"
+                  />
+                  <div className="minmax">
+                    <p>
+                      <span className="tempmax" />
+                      {parseInt(dia.day.maxtemp_c)}
+                    </p>
+                    <p>
+                      <span className="tempmin" />
+                      {parseInt(dia.day.mintemp_c)}
+                    </p>
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
-    );
+    ) : (
+      <div className="App">Cargando...</div>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Top.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+// El archivo css no es necesario cargarlo ya que se carga en el componente padre
+
+// Indicamos que la función recibe props
+const Top = (props) => {
+  // Usamos ES6 destructuring
+  const { current, location } = props;
+  return(
+    <div className="top">
+      <img src={current.condition.icon} alt="Clima principal" className="image" />
+      <p className="temp">{current.temp_c} °C</p>
+      <h4 className="city">
+        {location.name}, {location.region}, {location.country}
+      </h4>
+      <div className="feelslike">
+        ST: {current.feelslike_c} °C, H: {current.humidity} %
+      </div>
+    </div>
+  );
+};
+
+export default Top;
